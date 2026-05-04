ef6c0d4869fa02442f24ffc7055ff4a1ece4643a
Nisarg Shah <nisarg.shah.medium@gmail.com>
2026-05-04 23:40:39 +0530

    Fixed Team Booster Bug

--- Output.py
+++ Output.py
@@ -261,7 +261,7 @@ def run_output_pipeline():
                     if winner_booster_match == team_franchise:
                         franchise_points += 300
                     else:
-                        franchise_points -= 450
+                        franchise_points -= 600
         spreadsheet['Team Final Points'].setdefault(team, {})['Franchise Points'] = franchise_points
         spreadsheet['Team Final Points'].setdefault(team, {})['Franchise Wins'] = franchise_wins[team]
         print(f"{team}: {franchise_wins[team]} wins = {franchise_points} franchise points")
